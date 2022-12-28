4f3580c0b70e4b204e685a7f2e6af9a464dc443c
nicolas aizner <nicolasaizner@gmail.com>
2022-12-28 11:32:44 -0300

    Entrega_1_Final

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-
+// Prompt consultando Nombre, Apellido y Edad para validar que sea mayor de edad y además para capturar los datos y volcarlos luego al formulario que sigue
 
 let nombre = prompt("Ingresa tu nombre");
 let apellido = prompt("ingresa tu apellido");
 let edad = Number(prompt("Ingresa tu edad"));
 let correo = document.getElementById("correo").value
 
+// Validación de Edad mayor a 18 años / Acción de mostrar un Div oculto / Completando el formulario de inscripción
 if(isNaN(edad) || edad == ""){
     alert("Este campo solo permite números positivos");
 }else if (edad >= 18){
@@ -17,7 +18,7 @@ if(isNaN(edad) || edad == ""){
     alert("Sos menor de edad, no podes acceder al formulario de Inscripcion");
 }
 
-
+// Función en el formulario que se activa al ser enviado. Habilita los DIV con la información que sigue en la inscripción
 function clickFormulario() {
     alert("Los datos se han completado correctamente");
     document.getElementById('divResumen').style.display = "block";
@@ -26,10 +27,13 @@ function clickFormulario() {
     
 }
 
+// Selección de Sede para cursar estudios. 
+
 alert("A continuación elija la sede de su preferencia");
 
 let sede = prompt("Ingresa la Sede a inscribirse \n 1 para Sede CABA \n 2 para Sede Rosario \n 3 para Sede Córdoba");
 
+// Devuelve Dirección de sede elegida mediante un Switch
 const sedeCaba = "Av. Cordoba 5555 entre Quiroz y M. Belgrano";
 const sedeRosario = "Av Circumbalación 4569 esquina Medrano, Rosario";
 const sedeCordoba = "Av Corrientes 778 Villa Carlos Paz"
@@ -50,17 +54,18 @@ switch(sede) {
         break;
 }
 
-
+// Defino materias que componen cada plan de estudios a elegir
 let materiasFront = ["HTML", "CSS", "JAVASCRIPT", "ANGULAR", "REACT", "PROYECTO"];
 let materiasBack = ["JAVASCRIPT", "NODJS", "MySQL", "JAVA", "PROYECTO"];
 let materiasUx = ["DISEÑO UX", "REACT", "BOOTSTRAP", "SASS", "PROYECTO"]
 
 const planEstudios = document.getElementById('plan');
 
+// Defino una escucha para que cuando cambie el Select poder capturar el valor que elige el cliente. Para luego poder mostrarle las materias que incluye el Plan elegido
 planEstudios.addEventListener('change', (event) => {
     const resultado = document.getElementById('resultado');
     resultado.textContent = `Tu carrera elegida es ${event.target.value} y consta de las siguientes materias`;
-    
+//    Pregunto si la elección del usuario es X Carrera, con un For recorra el arreglo de esa carrera para mostrar las materias.
     if (event.target.value == "Front-End") {
         for (let i = 0; i < materiasFront.length; i++) {
             document.getElementById("materias").innerHTML += materiasFront[i] + " / ";
